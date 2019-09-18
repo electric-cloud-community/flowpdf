e301dad62331c58e44a89ff76c48761c3146111b
Polina Shubina <925043@gmail.com>
2019-09-18 14:01:24 +0300

    Added some checks for the correct file format/existing asset

--- SampleGithubREST/dsl/properties/perl/lib/FlowPlugin/SampleGithubREST.pm
+++ SampleGithubREST/dsl/properties/perl/lib/FlowPlugin/SampleGithubREST.pm
@@ -4,7 +4,10 @@ use strict;
 use warnings;
 use base qw/FlowPDF/;
 use FlowPDF::Log;
-
+use FlowPDF::Exception::RuntimeException;
+use FlowPDF::Log;
+use Try::Tiny;
+use FlowPDF::Helpers qw(bailOut);
 # Feel free to use new libraries here, e.g. use File::Temp;
 
 # Service function that is being used to set some metadata for a plugin.
@@ -77,30 +80,50 @@ sub createRelease {
         tag_name => $params->getParameter('tagName')->getValue,
         name => $r->{name},
     );
-    my $release = eval {
-        $client->getReleaseByTagName(%restParams, tag => $params->getParameter('tagName')->getValue);
+
+    my $release;
+    try {
+        # if there is no such release, this block will fail, but it is fine - we will create the release object
+        $release = $client->getReleaseByTagName(%restParams, tag => $params->getParameter('tagName')->getValue);
     };
 
+    my $stepResult = $context->newStepResult;
     if (!$release) {
         $release = $client->createRelease(%restParams);
     }
     logInfo "Release: ", $release;
-
-    my $releaseId = $release->{id};
-    my $asset = eval {
-        $client->uploadReleaseAsset(
-            %restParams,
-            releaseId => $releaseId,
-            assetPath => $params->getParameter('assetPath')->getValue,
-            assetName => $r->{assetName},
-        );
-    };
-    logInfo "Asset", $asset;
-    logInfo("Got response from the server: ", $release);
-
-    my $stepResult = $context->newStepResult;
+    my $releaseUrl = $release->{html_url};
+    $stepResult->setReportUrl("Release URL", $releaseUrl);
 
     $stepResult->apply();
+
+    my $releaseId = $release->{id};
+
+    my ($existingAsset) = grep { $_->{name} eq $r->{assetName} } @{$release->{assets}};
+    if ($existingAsset) {
+        $context->bailOut({
+            message => "The asset named $r->{assetName} already exists in the release $releaseId"
+        });
+    }
+
+    my $assetPath = $r->{assetPath};
+
+    unless(-f $assetPath){
+        $context->bailOut({
+            message => "The $assetPath should exist and should be a file (not a directory)",
+            suggestion => "Please check if $assetPath exists and is file"
+        });
+    }
+
+    my $asset = $client->uploadReleaseAsset(
+        %restParams,
+        releaseId => $releaseId,
+        assetPath => $assetPath,
+        assetName => $r->{assetName},
+    );
+
+    logInfo "Asset", $asset;
+    logInfo("Got response from the server: ", $release);
 }
 
 ## === step ends ===
