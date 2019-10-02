8062dffbe05b6b419ac5ef6bc32b77b1dbda55e2
Polina Shubina <925043@gmail.com>
2019-10-02 15:37:18 +0300

    Upgraded Sample JIRA plugin

--- SampleJIRA/dsl/promote.groovy
+++ SampleJIRA/dsl/promote.groovy
@@ -1,3 +1,5 @@
+
+// DO NOT EDIT THIS BLOCK === promote_autogen starts ===
 import groovy.transform.BaseScript
 import com.electriccloud.commander.dsl.util.BasePlugin
 
@@ -14,23 +16,17 @@ def pluginDir = getProperty("/projects/$pluginName/pluginDir").value
 
 //List of procedure steps to which the plugin configuration credentials need to be attached
 def stepsWithAttachedCredentials = [
-[procedureName: "Get Issue", stepName: "Get Issue"],
-// === steps with credentials ends ===
-// The code above will be updated automatically as you add more procedures into your plugin
-// Feel free to change the code below
+    [procedureName: "Get Issue", stepName: "Get Issue"],
+
 ]
 
-
 project pluginName, {
-    // Please do not remove the line below
-    property 'ec_keepFilesExtensions', value: 'true' // Needed to keep files extensions in the properties names (by default extensions are removed)
-
-    // This line is required in order for React forms to work
+    property 'ec_keepFilesExtensions', value: 'true'
     property 'ec_formXmlCompliant', value: 'true'
     loadPluginProperties(pluginDir, pluginName)
     loadProcedures(pluginDir, pluginKey, pluginName, stepsWithAttachedCredentials)
-    //plugin configuration metadata
-    // Please remove the block below if your plugin does not require configuration
+
+    // Plugin configuration metadata
     property 'ec_config', {
         configLocation = 'ec_plugin_cfgs'
         form = '$[' + "/projects/$pluginName/procedures/CreateConfiguration/ec_parameterForm]"
@@ -41,9 +37,18 @@ project pluginName, {
             }
         }
     }
-}
 
-// Copy existing plugin configurations from the previous
-// version to this version. At the same time, also attach
-// the credentials to the required plugin procedure steps.
-upgrade(upgradeAction, pluginName, otherPluginName, stepsWithAttachedCredentials)
+    }
+
+def retainedProperties = []
+
+upgrade(upgradeAction, pluginName, otherPluginName, stepsWithAttachedCredentials, 'ec_plugin_cfgs', retainedProperties)
+// DO NOT EDIT THIS BLOCK === promote_autogen ends, checksum: dae7f3a9b69964b619323574e6ca5817 ===
+// Do not edit the code above this line
+
+project pluginName, {
+    // You may add your own DSL instructions below this line, like
+    // property 'myprop', {
+    //     value: 'value'
+    // }
+}
